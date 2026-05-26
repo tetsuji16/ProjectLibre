92dc8aaa13f6770855aeb41a57ee15693ed14ed2
tetsuji16 <tetsuji.kato@gmail.com>
2026-05-27 00:01:20 +0900

    Clean Gradle configuration and tmp artifacts

--- projectlibre_core/build.gradle.kts
+++ projectlibre_core/build.gradle.kts
@@ -1,11 +1,5 @@
 dependencies {
     implementation(project(":projectlibre_contrib"))
-    implementation(
-        fileTree(rootProject.file("projectlibre_contrib/lib")) {
-            include("**/*.jar")
-            exclude("flamingo-6.2.jar", "trident-6.2.jar")
-        }
-    )
 }
 
 sourceSets {
--- projectlibre_ui/build.gradle.kts
+++ projectlibre_ui/build.gradle.kts
@@ -7,12 +7,6 @@ dependencies {
     implementation(project(":projectlibre_core"))
     implementation(project(":projectlibre_exchange"))
     implementation(project(":projectlibre_reports"))
-    implementation(
-        fileTree(rootProject.file("projectlibre_contrib/lib")) {
-            include("**/*.jar")
-            exclude("flamingo-6.2.jar", "trident-6.2.jar")
-        }
-    )
 }
 
 application {
