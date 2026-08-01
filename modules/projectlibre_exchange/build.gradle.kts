e5f00d0e99748442b91387f9bdd84d277a31a9b2
tetsuji16 <tetsuji.kato@gmail.com>
2026-08-01 16:43:01 +0900

    Stabilize POD round-trip test stack

--- modules/projectlibre_exchange/build.gradle.kts
+++ modules/projectlibre_exchange/build.gradle.kts
@@ -8,4 +8,7 @@ dependencies {
 tasks.test {
     enabled = true
     systemProperty("java.awt.headless", "true")
+    // POD restore walks task and dependency graphs recursively. Do not depend
+    // on the platform-specific default stack size of the test worker.
+    jvmArgs("-Xss2m")
 }
