9edcfc1d4bde1d805165d0d43a5530617f136e5a
tetsuji16 <57844720+tetsuji16@users.noreply.github.com>
2026-08-26 21:58:42 +0900

    refactor: drop unused KeyAdapter import in TabbedNavigation

--- modules/micrproject_ui/src/main/java/com/microproject/pm/graphic/TabbedNavigation.java
+++ modules/micrproject_ui/src/main/java/com/microproject/pm/graphic/TabbedNavigation.java
@@ -31,8 +31,6 @@ import java.awt.Font;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.io.Serializable;
 import java.util.ArrayList;
